02179b96ffadbd0b4fcbb38541f897d7e4a38455
Andrey Shchigartsov <a.shchigartsov@yandex.ru>
2026-03-18 22:02:04 +0300

    first

--- service/src/main/java/org/example/delivery/dto/CourierAssignmentRequest.java
+++
@@ -1,9 +0,0 @@
-package org.example.delivery.dto;
-
-import lombok.Data;
-import org.example.delivery.valueobject.CourierInfo;
-
-@Data
-public class CourierAssignmentRequest {
-    private CourierInfo courierInfo;
-}
